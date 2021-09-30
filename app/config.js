c05c8d81d031ba5e5d85ddc4205a6ccc38318b8c
Amanda <daijun.yu@digital.hmrc.gov.uk>
2021-09-30 17:22:33 +0100

    update

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'New name',
+  serviceName: 'this is a new name',
 
   // Default port that prototype runs on
   port: '3000',
