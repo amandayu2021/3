41dbf6511cde0d5aeb0ccc42555bc40847d56241
amandayu2021 daijun.yu@digital.hmrc.gov.uk <daijun.yu@digital.hmrc.gov.uk>
2021-10-18 23:29:32 +0100

    1

--- app/routes.js
+++ app/routes.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 
-// this is gray text area, just for reference for self, you can write as much as you want, Run this code when a form is submitted to 'juggling-balls-answer'
+// this is gray text area, just for reference for self, Run this code when a form is submitted to 'juggling-balls-answer'
 router.post('/juggling-balls-answer', function (req, res) {
 
   // Make a variable and give it the value from 'how-many-balls'
@@ -14,6 +14,41 @@ router.post('/juggling-balls-answer', function (req, res) {
   } else {
     // Send user to ineligible page
     res.redirect('/ineligible')
+  }
+
+})
+
+
+
+// thisis for the mutiple 02_multiplechoices router information'
+router.post('/02_multiplechoices', function (req, res) {
+
+  // Make a variable and give it the value from '02_multiplechoices'
+  let numberamanda = req.session.data['id-for-5'];
+
+  // Check whether the variable matches a condition
+  if (numberamanda === "1"){
+    // Send user to 1
+    res.redirect('/1')
+  }
+    if (numberamanda === "2"){
+      // Send user to 2
+      res.redirect('/2')
+  }
+      if (numberamanda === "3"){
+        // Send user to 2
+        res.redirect('/3')
+  }
+        if (numberamanda === "4"){
+          // Send user to 2
+          res.redirect('/4')
+  }
+          if (numberamanda === "5"){
+            // Send user to 2
+            res.redirect('/5')
+  } else {
+    // Send user to ineligible page
+    res.redirect('/start')
   }
 
 })
